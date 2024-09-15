a581b966b63238868f5adc36f85874af23b5e212
EriShoppe <rensengamboa@gmail.com>
2024-09-16 02:27:25 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -26,21 +26,6 @@ window.addEventListener('wheel', (event) => {
     }
 });
 
-const navLinks = document.querySelectorAll('nav a');
-
-navLinks.forEach(link => {
-    link.addEventListener('click', (event) => {
-        event.preventDefault();
-        const targetId = link.getAttribute('href').substring(1);
-        const targetSection = document.getElementById(targetId);
-        
-        targetSection.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    });
-});
-
 // Function to reveal sections on scroll
 function revealOnScroll() {
     const sections = document.querySelectorAll('.slide-section');
