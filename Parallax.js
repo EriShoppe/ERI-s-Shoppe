5fafe73a24a1fd7cecfd8ddac311b4582096686d
EriShoppe <rensengamboa@gmail.com>
2024-09-17 21:31:32 +0800

    Delete Parallax.js

--- Parallax.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const parallaxElements = document.querySelectorAll('.parallax');
-    parallaxElements.forEach(element => {
-        new Parallax(element);
-    });
-});
