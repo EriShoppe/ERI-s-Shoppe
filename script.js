3e25c35dd4f832cdfe0d8b3b9200aba5b9cae579
EriShoppe <rensengamboa@gmail.com>
2024-09-20 19:25:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -150,3 +150,22 @@ servicesSection.addEventListener('scroll', () => {
         scrollToSection(currentIndex + 1); // Scroll to the next section
     }
 });
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
